c2f93d1f471f4a582a9e4d71462b5a968c19fed9
Rick Mokros <rmokros@mokesit.com>
2015-10-06 09:56:39 -0400

    added route for grid example

---
+++ app/scripts/controllers/gridexample.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngSortApp.controller:GridexampleCtrl
+ * @description
+ * # GridexampleCtrl
+ * Controller of the ngSortApp
+ */
+angular.module('ngSortApp')
+  .controller('GridExampleCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
